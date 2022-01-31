04c623de82109f75ae9f0e64b7a7772e7c56984c
PallaviPrasanakumar <68636048+PallaviPrasanakumar@users.noreply.github.com>
2022-01-30 23:21:54 -0800

    Change verbiage

--- public/App.js
+++ public/App.js
@@ -1,4 +1,4 @@
-const NAME = "Himani Mishra";
+const NAME = "Pallavi Prasanna Kumar";
 
 const element = React.createElement(
 	"div",
@@ -8,11 +8,11 @@ const element = React.createElement(
 		{ className: "border" },
 		NAME
 	),
-	React.createElement("img", { className: "border", src: "https://static-beautyhigh.stylecaster.com/2014/05/477973067.jpg", width: "300px" }),
+	React.createElement("img", { className: "border", src: "https://media-exp1.licdn.com/dms/image/C5603AQETePBDMI7E5w/profile-displayphoto-shrink_800_800/0/1517243299585?e=1649289600&v=beta&t=t1Pu0GR2WBaSxEK8ejjkgeQcKRrDA43M_MFup_V3XbA", width: "300px" }),
 	React.createElement(
 		"p",
 		null,
-		"A Fullstack Web Developer from San Diego. International Graduate Student at San Diego State University pursuing Masters in Computer Science with an expected graduation date of May 2023. Actively seeking internships in Software Development for Summer 2022."
+		"A Fullstack Web Developer from San Diego. International Graduate Student at San Diego State University pursuing Masters in Computer Science with an expected graduation date of Dec 2023. Actively seeking internships in Software Development for Summer 2023."
 	),
 	React.createElement(
 		"a",
--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ const element = (
 	<div className="container border">
 		<h1 className="border">{NAME}</h1>
 		<img className="border" src="https://media-exp1.licdn.com/dms/image/C5603AQETePBDMI7E5w/profile-displayphoto-shrink_800_800/0/1517243299585?e=1649289600&v=beta&t=t1Pu0GR2WBaSxEK8ejjkgeQcKRrDA43M_MFup_V3XbA" width="300px" />
-		<p>A Fullstack Web Developer from San Diego. International Graduate Student at San Diego State University pursuing Masters in Computer Science with an expected graduation date of May 2023. Actively seeking internships in Software Development for Summer 2022.</p>
+		<p>A Fullstack Web Developer from San Diego. International Graduate Student at San Diego State University pursuing Masters in Computer Science with an expected graduation date of Dec 2023. Actively seeking internships in Software Development for Summer 2023.</p>
 		<a href="https://github.com/himani-sdsu" target="_blank">
 			<button className="border"><h3>VIEW MY GITHUB REPO</h3></button>
 		</a>
